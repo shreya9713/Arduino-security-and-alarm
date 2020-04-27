ee316f549684c6872f6ca699125f32330b58c515
shreya9713 <60899067+shreya9713@users.noreply.github.com>
2020-04-27 12:51:26 -0500

    Add files via upload

---
+++ DisplayLCD.ino
@@ -0,0 +1,10 @@
+// Display in LCD
+void DisplayLCD()  
+{
+
+lcd.setCursor(0,0); // set cursor to (0,0)
+lcd.println("***Please enter password***"); // Print enter the passcode
+lcd.setCursor(1 ,1);
+
+lcd.println("OPEN _/_Display!!"); // Display on screen
+}
